1733ea4315f5f6e5da5e8e85523255f67b054630
Rakib Hossain <elegantrakib@gmail.com>
2019-09-18 17:19:08 +0600

    add transaction roolback in user create

--- user/models.py
+++ user/models.py
@@ -16,14 +16,25 @@ class UserManager(BaseUserManager):
 		return hashlib.sha256(salt.encode() + password.encode()).hexdigest()
 
 	def _create_user(self, email, password, **extra_fields):
-		"""Create and save a User with the given email and password."""
-		if not email:
-			raise ValueError('The given email must be set')
-		email = self.normalize_email(email)
-		user = self.model(email=email, **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
+		sid = transaction.savepoint()
+
+		try:
+			"""Create and save a User with the given email and password."""
+			if not email:
+				raise ValueError('The given email must be set')
+			email = self.normalize_email(email)
+			user = self.model(email=email, **extra_fields)
+			user.set_password(password)
+			user.save(using=self._db)
+
+			transaction.savepoint_commit(sid)
+
+			return user
+		except IntegrityError as e:
+			transaction.savepoint_rollback(sid)
+			print(e.message)
+			
+			return False
 
 	def create_user(self, email, password=None, **extra_fields):
 		"""Create and save a regular User with the given email and password."""
